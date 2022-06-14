195b6005beea9eedfee6c4a659ef8859194acc73
barcelosluan <contato@barcelosluan.dev>
2022-06-14 01:08:14 -0300

    fix: erro ao tentar ler um arquivo que não existia

--- src/core/files/share/get_all.py
+++ src/core/files/share/get_all.py
@@ -30,27 +30,25 @@ def get_all(req: Request):
         file_name = file['file']
         file_path = f'/home/{file_dir}/{file_name}'
         
-        if not path.exists(file_path):
-            continue
-        
-        f = {
-            'name': file_name,
-            'owner': file_dir,
-            'size': convert_size(path.getsize(file_path)),
-            'uploaded': int(path.getmtime(file_path)),
-            'extension': None
-        }
-        
-        if path.isdir(file_path):
-            f['extension'] = 'directory'
-        elif '.' in file_name:
-            extension = file_name.split('.')
+        if path.exists(file_path):
+            f = {
+                'name': file_name,
+                'owner': file_dir,
+                'size': convert_size(path.getsize(file_path)),
+                'uploaded': int(path.getmtime(file_path)),
+                'extension': None
+            }
             
-            if file_name[0] == '.' and len(extension) > 2:
-                f['extension'] = extension[-1]
-            elif file_name[0] != '.' and len(extension) > 1:
-                f['extension'] = extension[-1]
-        
-        retorno.append(f)
+            if path.isdir(file_path):
+                f['extension'] = 'directory'
+            elif '.' in file_name:
+                extension = file_name.split('.')
+                
+                if file_name[0] == '.' and len(extension) > 2:
+                    f['extension'] = extension[-1]
+                elif file_name[0] != '.' and len(extension) > 1:
+                    f['extension'] = extension[-1]
+            
+            retorno.append(f)
     
     return retorno
